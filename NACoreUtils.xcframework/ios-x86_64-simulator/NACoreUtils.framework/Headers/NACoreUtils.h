891386063b2399a50cc858e82d9b78c672bcfb98
HoangSon <Codetts@gmail.com>
2022-09-20 14:16:36 +0700

    - releae universal sdk v0.0.9

---
+++ NACoreUtils.xcframework/ios-x86_64-simulator/NACoreUtils.framework/Headers/NACoreUtils.h
@@ -0,0 +1,18 @@
+//
+//  NACoreUtils.h
+//  NACoreUtils
+//
+//  Created by Tran Phong on 3/9/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NACoreUtils.
+FOUNDATION_EXPORT double NACoreUtilsVersionNumber;
+
+//! Project version string for NACoreUtils.
+FOUNDATION_EXPORT const unsigned char NACoreUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NACoreUtils/PublicHeader.h>
+
+
---
+++ NALocalization.xcframework/ios-x86_64-simulator/NALocalization.framework/Headers/NALocalization.h
@@ -0,0 +1,18 @@
+//
+//  NALocalization.h
+//  NALocalization
+//
+//  Created by Tran Phong on 4/19/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NALocalization.
+FOUNDATION_EXPORT double NALocalizationVersionNumber;
+
+//! Project version string for NALocalization.
+FOUNDATION_EXPORT const unsigned char NALocalizationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NALocalization/PublicHeader.h>
+
+
